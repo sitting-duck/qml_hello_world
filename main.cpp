3477ed16ca23504126aea3a41797bb8ffb1d4bf9
Ashley <ashley.tharp@gmail.com>
2017-11-18 11:40:32 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
+#ifdef _WIN32
+#include "../../zlib/include/contrib/minizip/zip.h"
+#include "../../zlib/include/contrib/minizip/unzip.h"
+#else
+#include "zlib.h"
+//#include "/usr/local/include/minizip/zip.h"
+//#include "/usr/local/include/minizip/unzip.h"
+#include "OpenImageIO/version.h"
+#endif
+
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
